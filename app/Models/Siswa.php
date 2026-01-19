5a375944efab52ebfc3369d39ad3c66209121c8d
vitaayuputry <vitaayuputri23@gmail.com>
2026-01-19 08:31:09 +0700

    initial commit

---
+++ app/Models/Siswa.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Siswa extends Model
+{
+    protected $table = 'siswa';
+
+    protected $fillable = [
+        'nisn',
+        'nama_siswa',
+        'jenis_kelamin',
+        'kelas_id'
+    ];
+}
